0ae0f04de5319ccb579acf0328104737b1ac903e
Wendril Adam <adamwendril@gmail.com>
2020-10-15 15:32:03 -0300

    Adicionado enum TipoConta

---
+++ Models/Enum/TipoConta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financial.Models.Enum
+{
+    public enum TipoConta
+    {
+        Corrente = 0,
+        Poupanca = 1,
+        Salario = 3
+    }
+}
---
+++ Models/IConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financial.Models
+{
+    interface IConta
+    {
+        decimal VizualisarSaldo();
+        decimal GerarSaldo(decimal valor);
+    }
+}
